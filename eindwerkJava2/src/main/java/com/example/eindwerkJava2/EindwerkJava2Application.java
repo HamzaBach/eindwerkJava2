e1590eb4ad96f3240f4e09c0b4116ad8c4953e40
Hamza Bachiri <h.bachiri@scia.net>
2022-01-17 21:06:31 +0100

    Initial commit

---
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/EindwerkJava2Application.java
@@ -0,0 +1,13 @@
+package com.example.eindwerkJava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindwerkJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindwerkJava2Application.class, args);
+	}
+
+}
