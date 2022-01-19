ac51cd173731444b67aed86da9c8fd2da2f09faf
Hamza Bachiri <h.bachiri@scia.net>
2022-01-19 19:50:10 +0100

    unfinished deve

---
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/model/Article.java
@@ -0,0 +1,10 @@
+package com.example.eindwerkJava2.model;
+
+public class Article {
+
+    private Long articleId;
+    private String articleName;
+    private String articleDescription;
+
+
+}
---
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/model/Category.java
@@ -0,0 +1,6 @@
+package com.example.eindwerkJava2.model;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+}
