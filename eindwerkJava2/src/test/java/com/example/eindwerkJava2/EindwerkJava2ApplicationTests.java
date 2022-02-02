83e9f330670395ab286b84ca506d0692f718f357
Hamza Bachiri <h.bachiri@scia.net>
2022-02-02 18:00:53 +0100

    Revert "Merge remote-tracking branch 'origin/master' into Articles_and_Mutation"
    This reverts commit 5b686af138c564a81dd0822a5004b49ea554964f, reversing
    changes made to 556ae526d92175746f401616beebf2c3932eebd2.

---
+++ eindwerkJava2/src/test/java/com/example/eindwerkJava2/EindwerkJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindwerkJava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindwerkJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
