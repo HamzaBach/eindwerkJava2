ec70e4861402e5bb778b0093a2e2565a0fd9da3d
Abdel Arfala <abdeljalil.arfala@gmail.com>
2022-03-07 18:56:10 +0100

    Final

---
+++ demo/src/main/java/tool.java
@@ -0,0 +1,18 @@
+//import model.Card;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import com.example.demo.repositories.CardRepository;
+//
+//@Configuration
+//public class tool {
+//    @Bean
+//    CommandLineRunner commandLineRunner(CardRepository cardRepository){
+//
+//        return args ->{
+//            Card test = new Card(1l,"Title","Text",0);
+//            cardRepository.save(test);
+//        };
+//    }
+//
+//}
