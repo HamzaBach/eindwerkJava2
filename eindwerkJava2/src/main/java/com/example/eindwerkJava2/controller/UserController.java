a1c6ca802e91a839b4399be39b9c24e830691c94
Hamza Bachiri <h.bachiri@scia.net>
2022-03-07 21:37:47 +0100

    Added Users with roles to application.

--- eindwerkJava2/src/main/java/com/example/eindwerkJava2/controller/UserController.java
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.eindwerkJava2.controller;
 
+import com.example.eindwerkJava2.model.Article;
+import com.example.eindwerkJava2.model.EmployeeRole;
 import com.example.eindwerkJava2.model.User;
 import com.example.eindwerkJava2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -24,6 +29,49 @@ public class UserController {
         List<User> users = userService.getUsers();
         model.addAttribute("UsersList",users);
         return "users";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user,
+                              @RequestParam("image") MultipartFile multipartFile) throws IOException {
+        byte[] addedImage = multipartFile.getBytes();
+        this.userService.saveUser(user,addedImage);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user/image/{userId}")
+    @ResponseBody
+    void showImage(@PathVariable("userId") Long userId, HttpServletResponse response, Optional<User> user)
+            throws IOException {
+        user = userService.findById(userId);
+        if(user.get().getUserImage()!=null){
+            response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+            response.getOutputStream().write(user.get().getUserImage());
+            response.getOutputStream().close();
+        }
+    }
+
+    @GetMapping("editUser/{userId}")
+    public String showEditUserForm(@PathVariable("userId") Long userId, Model model) {
+        User user = userService.findById(userId).get();
+        model.addAttribute("rolesList", EmployeeRole.values());
+        model.addAttribute("user", user);
+        return "form_user";
+    }
+
+
+    @GetMapping("deleteUser/{userId}")
+    public String deleteUser(@PathVariable("userId") Long userId){
+        User user = userService.findById(userId).get();
+        this.userService.deleteUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/showNewUserForm")
+    public String showNewUserForm(Model model) {
+        model.addAttribute("user",new User());
+        model.addAttribute("rolesList", EmployeeRole.values());
+        return "form_user";
     }
 
 /*    @GetMapping("/showNewUserForm")
