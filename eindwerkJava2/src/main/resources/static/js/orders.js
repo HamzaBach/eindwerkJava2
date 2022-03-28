d2c4d90fe754356d7f3598c2bcc344a225a2e4a2
Hamza Bachiri <benibouhayie@hotmail.com>
2022-03-28 21:11:25 +0200

    Added spring security access rights for create/edit/delete based on user's role

---
+++ eindwerkJava2/src/main/resources/static/js/orders.js
@@ -0,0 +1,15 @@
+//function EnableDisableSaveHeader() {
+//        //Reference the Button.
+//        var btnSubmit = document.getElementById("saveOrderHeader");
+//        var orderNumber = document.getElementById("orderNumber");
+//
+//        //Verify the OrderNumber has an order number.
+//        if (orderNumber.value.trim() != "Order nr°") {
+//            //Disable the Save header button when an order number is present.
+//            btnSubmit.disabled = true;
+//        } else {
+//            //Disable the TextBox when TextBox is empty.
+//            btnSubmit.disabled = false;
+//        }
+//    };
+
