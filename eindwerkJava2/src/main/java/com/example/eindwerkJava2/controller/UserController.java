c3c3d4cfcbb0305d1c805474398552aaa387662b
Hamza Bachiri <benibouhayie@hotmail.com>
2022-04-01 12:01:55 +0200

    Javadoc Users and roles

--- eindwerkJava2/src/main/java/com/example/eindwerkJava2/controller/UserController.java
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/controller/UserController.java
@@ -14,39 +14,80 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The controller layer for connecting the front-end with the back-end for users.
+ *
+ * @author Hamza Bachiri
+ * @version 1.0
+ */
+
 @Controller
 @RequestMapping
 public class UserController {
+    /**
+     * Autowiring of the UserService from the IOC container.
+     */
     @Autowired
     private final UserService userService;
+    /**
+     * Autowiring of the RolesService from the IOC container.
+     */
     @Autowired
     private final RolesService rolesService;
 
-    public UserController(UserService userService, RolesService rolesService){
-        this.userService=userService;
-        this.rolesService=rolesService;
+    /**
+     * Dependency injection of the UserService & RolesService into the constructor of the UserController.
+     *
+     * @param userService
+     * @param rolesService
+     */
+    public UserController(UserService userService, RolesService rolesService) {
+        this.userService = userService;
+        this.rolesService = rolesService;
     }
 
+    /**
+     * Endpoint (GET) that returns all the users.
+     *
+     * @param model Model is used for adding the UsersList as an attribute of the model to be accessed by the front-end.
+     * @return Users html template is returned with an overview of all active users.
+     */
     @GetMapping("/users")
-    public String getUsers(Model model){
+    public String getUsers(Model model) {
         List<User> users = userService.getActiveUsers();
-        model.addAttribute("UsersList",users);
+        model.addAttribute("UsersList", users);
         return "users";
     }
 
+    /**
+     * Endpoint (POST) that saves a new user.
+     *
+     * @param user          Object that is used for storing the new user in the persistence layer via the UserService.
+     * @param multipartFile This is the picture of the user that is stored in the persistence layer via the UserService.
+     * @return After a successful save the users overview is returned.
+     * @throws IOException Exception is triggered in case the save action failed.
+     */
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user,
-                              @RequestParam("image") MultipartFile multipartFile) throws IOException {
+                           @RequestParam("image") MultipartFile multipartFile) throws IOException {
         byte[] addedImage = multipartFile.getBytes();
-        this.userService.saveUser(user,addedImage);
+        this.userService.saveUser(user, addedImage);
         return "redirect:/users";
     }
 
+    /**
+     * Endpoint (GET) that returns the image of the user.
+     *
+     * @param userId   The id of the user for retrieving it from the persistence layer.
+     * @param response It is used for preparing the image to be used in the front-end.
+     * @param user     The user object that is used for obtaining the user's image.
+     * @throws IOException Exception is triggered in case the retrieval of the image goes wrong.
+     */
     @GetMapping("/user/image/{userId}")
     @ResponseBody
     void showImage(@PathVariable("userId") Long userId, HttpServletResponse response, Optional<User> user)
             throws IOException {
-        if(user.get().getUserImage()!=null){
+        if (user.get().getUserImage() != null) {
             user = userService.findById(userId);
             response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
             response.getOutputStream().write(user.get().getUserImage());
@@ -54,6 +95,13 @@ public class UserController {
         }
     }
 
+    /**
+     * Endpoint (GET) for editing a user's settings and roles.
+     *
+     * @param userId The id of the user for retrieving it from the persistence layer.
+     * @param model  Model is used for adding the Roles, UserRoles and UserNotRoles as an attribute of the model to be accessed by the front-end.
+     * @return The form containing the settings of the user is returned.
+     */
     @GetMapping("edit/user/{userId}")
     public String showEditUserForm(@PathVariable("userId") Long userId, Model model) {
         User user = userService.findById(userId).get();
@@ -64,20 +112,29 @@ public class UserController {
         return "/forms/form_user";
     }
 
-
+    /**
+     * Endpoint (GET) for deleting an user from the persistence layer.
+     *
+     * @param userId The id of the user for retrieving it from the persistence layer.
+     * @return After a successful delete the users overview is returned.
+     */
     @GetMapping("delete/user/{userId}")
-    public String deleteUser(@PathVariable("userId") Long userId){
+    public String deleteUser(@PathVariable("userId") Long userId) {
         User user = userService.findById(userId).get();
         this.userService.deleteUser(user);
         return "redirect:/users";
     }
 
+    /**
+     * Endpoint (GET) for adding a new user to the persistence layer.
+     *
+     * @param model Model is used for adding the host user object and a list of all available roles to be used in the front-end.
+     * @return The form for adding a new user is returned.
+     */
     @GetMapping("/new/user")
     public String showNewUserForm(Model model) {
-        model.addAttribute("user",new User());
+        model.addAttribute("user", new User());
         model.addAttribute("rolesList", rolesService.getAllRoles());
         return "/forms/form_user";
     }
-
-
 }
--- eindwerkJava2/src/main/java/com/example/eindwerkJava2/service/UserService.java
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/service/UserService.java
@@ -11,63 +11,110 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This class represents the service layer for users.
+ *
+ * @author Hamza Bachiri
+ * @version 1.0
+ */
 @Service
 public class UserService {
+    /**
+     * Autowiring of the UserRepository from the IOC container.
+     */
     @Autowired
     private final UserRepository userRepository;
+    /**
+     * Autowiring of the RoleRepository from the IOC container.
+     */
     @Autowired
     private final RoleRepository roleRepository;
+    /**
+     * Autorwiring of the BcryptPasswordEncoder from the IOC container.
+     */
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-    private static final Role DEFAULT_ROLE=new Role("USER");
+    private static final Role DEFAULT_ROLE = new Role("USER");
 
+    /**
+     * Dependency injection of the UserRepository and RoleRepository into the UserService.
+     *
+     * @param userRepository The to be injected UserRepository.
+     * @param roleRepository The to be injected RoleRepository.
+     */
     public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
     }
 
+    /**
+     * Getter method for getting the active user value.
+     *
+     * @return The active user value is returned.
+     */
     public List<User> getActiveUsers() {
         return this.userRepository.findByActiveUser(1);
     }
 
-
+    /**
+     * Getter method for getting the user name.
+     *
+     * @param userName The to be retrieved user name.
+     * @return The retrieved user object is returned.
+     */
     public User getUserByUserName(String userName) {
         if (userRepository.existsUserByUserName(userName)) {
             User retrievedUser = userRepository.findByUserName(userName);
             return retrievedUser;
-        } else throw new IllegalStateException("User with username: "+userName+" could not been found.");
+        } else throw new IllegalStateException("User with username: " + userName + " could not been found.");
     }
 
-    public void saveUser(User user, byte[] userImage){
-        if(userImage.length==0)
-        {
-            if(userRepository.existsUserByUserId(user.getUserId())){
+    /**
+     * Method for saving or updating a user.
+     *
+     * @param user      The to be saved user.
+     * @param userImage The image of the user that is saved within the user object.
+     */
+    public void saveUser(User user, byte[] userImage) {
+        if (userImage.length == 0) {
+            if (userRepository.existsUserByUserId(user.getUserId())) {
                 User currentUser = userRepository.getById(user.getUserId());
                 user.setUserImage(currentUser.getUserImage());
             }
-        }else{
+        } else {
             user.setUserImage(userImage);
         }
 
-        if(user.getUserId()==null){
+        if (user.getUserId() == null) {
             user.addOneRole(roleRepository.findById(1).get());
-            String encryptedPassword=passwordEncoder.encode(user.getPassword());
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
             user.setPassword(encryptedPassword);
-        }else{
+        } else {
             user.setRoles(userRepository.findById(user.getUserId()).get().getRoles());
         }
         userRepository.save(user);
     }
 
-    public Optional<User> findById(Long id){
-        if(userRepository.existsUserByUserId(id)){
+    /**
+     * Method to find a user from the persistence layer.
+     *
+     * @param id The user id for obtaining the user object.
+     * @return The user object is returned.
+     */
+    public Optional<User> findById(Long id) {
+        if (userRepository.existsUserByUserId(id)) {
             return userRepository.findById(id);
-        }else{
+        } else {
             return null;
         }
     }
 
-    public void deleteUser(User user){
+    /**
+     * Method for deleting a user by setting the active user = 0.
+     *
+     * @param user The to be deleted user object.
+     */
+    public void deleteUser(User user) {
         user.setActiveUser(0);
         this.userRepository.save(user);
     }
