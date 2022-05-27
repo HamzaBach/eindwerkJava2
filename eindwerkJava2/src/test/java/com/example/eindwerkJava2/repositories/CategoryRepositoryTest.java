c5f923b0c4da16e3c05a8c3d87bec1b1c95fcc65
Hamza Bachiri <h.bachiri@scia.net>
2022-05-27 21:14:15 +0200

    Added working unit test (articleSupplierRepository & categoryRepository)

---
+++ eindwerkJava2/src/test/java/com/example/eindwerkJava2/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.eindwerkJava2.repositories;
+
+import com.example.eindwerkJava2.model.Category;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class CategoryRepositoryTest {
+    @Autowired
+    CategoryRepository categoryRepositoryTest;
+
+    @BeforeEach
+    void dummyData(){
+        Category smartphone = new Category("Smartphone", "SMTPH");
+        smartphone.setActive(0);
+        Category smartphoneAccessoires = new Category("Smartphone Accessoires", "SMTPH-ACC");
+        smartphoneAccessoires.setActive(1);
+        Category laptopAccesoires = new Category("Laptops", "LPTP");
+        laptopAccesoires.setActive(1);
+        categoryRepositoryTest.save(smartphone);
+        categoryRepositoryTest.save(smartphoneAccessoires);
+        categoryRepositoryTest.save(laptopAccesoires);
+    }
+
+    @Test
+    void findAllActiveCategories() {
+        //Given+When see dummyData
+        //Then
+        assertEquals(2,categoryRepositoryTest.findAllActiveCategories().size());
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        categoryRepositoryTest.deleteAll();
+    }
+}
