9fe6311c61b816b92ad365acd47c6a78ab551881
Hamza Bachiri <h.bachiri@scia.net>
2022-05-27 14:42:48 +0200

    refactoring user page (password change use case)

--- eindwerkJava2/src/main/java/com/example/eindwerkJava2/model/dto/UserDto.java
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/model/dto/UserDto.java
@@ -1,16 +1,35 @@
 package com.example.eindwerkJava2.model.dto;
 
+import com.example.eindwerkJava2.model.Role;
+import com.example.eindwerkJava2.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserDto {
-    private String userName;
-    private String password;
-    private String confirmPassword;
+public class UserDto extends User {
+
     private String newPassword;
+    private String confirmPassword;
+
+    public void convertUserToDTO(User user){
+        setUserId(user.getUserId());
+        setUserName(user.getUserName());
+        setUserImage(user.getUserImage());
+        setRoles(user.getRoles());
+    }
+    public User convertDTOtoUser(){
+        User user = new User();
+        user.setUserId(this.getUserId());
+        user.setUserName(this.getUserName());
+        user.setPassword(this.getPassword());
+        user.setActiveUser(this.getActiveUser());
+        user.setUserImage(this.getUserImage());
+        user.setRoles(this.getRoles());
+        return user;
+    }
 
 }
