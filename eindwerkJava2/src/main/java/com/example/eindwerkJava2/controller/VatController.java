8d9caf5c3439f0fa407e9e2b51947e0c0fdc2f0a
HamzaBach <benibouhayie@hotmail.com>
2022-06-12 01:16:47 +0200

    Added VAT view, controller

---
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/controller/VatController.java
@@ -0,0 +1,93 @@
+package com.example.eindwerkJava2.controller;
+
+import com.example.eindwerkJava2.model.Category;
+import com.example.eindwerkJava2.model.Vat;
+import com.example.eindwerkJava2.service.CategoryService;
+import com.example.eindwerkJava2.service.VatService;
+import com.example.eindwerkJava2.wrappers.SuccessEvaluator;
+import com.example.eindwerkJava2.wrappers.SuccessObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+@Controller
+
+public class VatController {
+    @Autowired
+    private final VatService vatService;
+
+
+    public VatController(VatService vatService){
+        this.vatService=vatService;
+    }
+
+
+    @GetMapping("/vat_rates")
+    public String viewCategories(Model model){
+        SuccessEvaluator<Vat> success = vatService.getAllActiveVats();
+        if(success.getIsSuccessfull()){
+            model.addAttribute("vatRatesList", success.getEntities());
+        } else{
+            model.addAttribute("error",success.getMessage());
+        }
+        return "vat_rates";
+    }
+
+    @GetMapping("/new/vat_rate")
+    public String showNewVatForm(Model model) {
+        SuccessEvaluator<Vat> success = vatService.getAllActiveVats();
+        if (success.getIsSuccessfull()) {
+            model.addAttribute("vatRatesList", success.getEntities());
+            model.addAttribute("vatObject", new Vat());
+        } else {
+            model.addAttribute("error", success.getMessage());
+        }
+        return "form_vat_rate";
+    }
+
+    @PostMapping("/saveVatRate")
+    public String saveVatRate(@ModelAttribute("vatObject") Vat vat, RedirectAttributes redirAttrs, Model model){
+        SuccessObject isSaveSuccessfull = this.vatService.save(vat);
+        if(isSaveSuccessfull.getIsSuccessfull()){
+            redirAttrs.addFlashAttribute("success",isSaveSuccessfull.getMessage());
+            return "redirect:/vat_rates";
+        } else {
+            model.addAttribute("error", isSaveSuccessfull.getMessage());
+            return "/forms/form_vat_rate";
+        }
+    }
+
+
+    @GetMapping("edit/vat_rate/{vatId}")
+    public String showEditVatForm(@PathVariable("vatId") Long vatId, Model model){
+        SuccessEvaluator<Vat> success = vatService.findById(vatId);
+        if(success.getIsSuccessfull()){
+            Vat vatRate = success.getEntity();
+            model.addAttribute("vatObject", vatRate);
+            model.addAttribute("vatList", vatService.getAllActiveVats());
+        } else {
+            model.addAttribute("error", success.getMessage());
+        }
+        return "/forms/form_vat_rate";
+    }
+
+    @GetMapping("delete/vat_rate/{vatId}")
+    public String deleteVatRate(@PathVariable("vatId") Long vatId, RedirectAttributes redirAttrs){
+        SuccessEvaluator<Vat> findVatRate = vatService.findById(vatId);
+        if(findVatRate.getIsSuccessfull()){
+            SuccessObject toBeDeletedVatRate = this.vatService.deleteVatRate((findVatRate.getEntity()));
+            if(toBeDeletedVatRate.getIsSuccessfull()){
+                redirAttrs.addFlashAttribute("success", toBeDeletedVatRate.getMessage());
+            } else{
+                redirAttrs.addFlashAttribute("error", toBeDeletedVatRate.getMessage());
+            }
+        } else {
+            redirAttrs.addFlashAttribute("error", findVatRate.getMessage());
+        }
+        return "redirect:/vat_rates";
+    }
+
+}
--- eindwerkJava2/src/main/java/com/example/eindwerkJava2/service/VatService.java
+++ eindwerkJava2/src/main/java/com/example/eindwerkJava2/service/VatService.java
@@ -1,11 +1,14 @@
 package com.example.eindwerkJava2.service;
 
+import com.example.eindwerkJava2.model.Category;
 import com.example.eindwerkJava2.model.Vat;
 import com.example.eindwerkJava2.repositories.VatRepository;
 import com.example.eindwerkJava2.wrappers.SuccessEvaluator;
+import com.example.eindwerkJava2.wrappers.SuccessObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,22 +20,32 @@ public class VatService {
     public VatService(VatRepository vatRepository){
         this.vatRepository=vatRepository;
     }
+    private static final DecimalFormat df = new DecimalFormat("0.00");  // rounding a double to 2 decimal points
 
     public SuccessEvaluator<Vat> save(Vat vat){
+
         SuccessEvaluator<Vat> isSaveSuccess = new SuccessEvaluator<>();
         if(vatRepository.existsVatByVatRate(vat.getVatRate())){
-            isSaveSuccess.setMessage("There already exists a vatrate for: "+vat.getVatRate()*100+"%");
+            isSaveSuccess.setMessage("There already exists a VAT rate for: "+df.format(vat.getVatRate()*100)+"%");
             isSaveSuccess.setIsSuccessfull(false);
         }else{
             vatRepository.save(vat);
-            isSaveSuccess.setMessage("Vatrate of"+vat.getVatRate()*100+"% has been succesfully saved.");
+            isSaveSuccess.setMessage("VAT rate of"+df.format(vat.getVatRate()*100)+" % has been succesfully saved.");
             isSaveSuccess.setIsSuccessfull(true);
         }
         return isSaveSuccess;
     }
 
-    public List<Vat> getAllActiveVats(){
-        return vatRepository.findByActive(1);
+    public SuccessEvaluator<Vat> getAllActiveVats(){
+        SuccessEvaluator<Vat> getActiveVatRates = new SuccessEvaluator<>();
+        if(vatRepository.findByActive(1).size()>0){
+            getActiveVatRates.setIsSuccessfull(true);
+            getActiveVatRates.setEntities(vatRepository.findByActive(1));
+        }else {
+            getActiveVatRates.setIsSuccessfull(false);
+            getActiveVatRates.setMessage("No VAT rates found within the database.");
+        }
+        return getActiveVatRates;
     }
 
     public boolean existsVatrate(Vat vat){
@@ -42,6 +55,33 @@ public class VatService {
         } else {
             return duplicateVat.get().getActive() == 1;
         }
+    }
+
+    public SuccessEvaluator<Vat> findById(long id){
+        SuccessEvaluator<Vat> success = new SuccessEvaluator<>();
+        if(vatRepository.findById(id).isEmpty()){
+            success.setIsSuccessfull(false);
+            success.setMessage("VAT rate not found!");
+        } else {
+            Vat retrievedVatRate = vatRepository.findById(id).get();
+            success.setEntity(retrievedVatRate);
+            success.setIsSuccessfull(true);
+        }
+        return success;
+    }
+
+    public SuccessObject deleteVatRate(Vat vat) {
+        SuccessObject success = new SuccessEvaluator<>();
+        vat.setActive(0);
+        this.vatRepository.save(vat);
+        if(vatRepository.findById(vat.getVatId()).get().getActive()==0){
+            success.setIsSuccessfull(true);
+            success.setMessage("VAT rate "+ df.format(vat.getVatRate())+" (ID: "+vat.getVatId()+" ) was successfully removed");
+        } else{
+            success.setIsSuccessfull(false);
+            success.setMessage("VAT rate " + df.format(vat.getVatRate())+" (ID: "+vat.getVatId()+" )" + " could not be removed.");
+        }
+        return success;
     }
 
 }
